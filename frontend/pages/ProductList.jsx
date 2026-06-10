ba50db6ed80a5544161b18d9699238aa2a6de14c
aryan <aryanjoshi081@gmail.com>
2026-06-10 22:13:02 +0545

    update

---
+++ frontend/pages/ProductList.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import ProductCard from "../components/ProductCard";
+
+function ProductList() {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const BASE_URL = import.meta.env.VITE_DJANGO_BASE_URL || "http://127.0.0.1:8000";
+    useEffect(() => {
+        fetch(`${BASE_URL}/api/products/`)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Failed to fetch products");
+                }
+                return response.json();
+            })
+            .then((data) => {
+                setProducts(data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }, [BASE_URL]);
+
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <div className="min-h-screen bg-gray-100 p-4">
+<h1 className="text-2xl font-bold  text-center py-6 bg-white shadow-md">Products List:</h1>
+<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-6">
+    {products.length > 0 ? (
+        products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+        ))
+    ) : (
+        <p className="col-span-full text-center text-gray-500">No products available.</p>
+    )}
+</div>
+</div>
+);  
+
+export default ProductList;
